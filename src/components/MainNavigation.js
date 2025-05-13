f5589e3ef697ce101ad69853fd1f689b4b0ca6e2
Kapil Jugnu <kr_kapil@hotmail.com>
2025-05-13 12:01:20 +0530

    nav link for active link

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import classes from './MainNavigation.module.css';
 export default function MainNavigation() {
   return <header className={classes.header}>
     <nav>
       <ul className={classes.list}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/products">Products</Link></li>
+        <li><NavLink to="/" className={({ isActive }) => isActive? classes.active : undefined}>Home</NavLink></li>
+        <li><NavLink to="/products" className={({ isActive }) => isActive? classes.active : undefined}>Products</NavLink></li>
       </ul>
     </nav>
   </header>
