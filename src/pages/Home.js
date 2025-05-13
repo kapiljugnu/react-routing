ce46efd5801eb30c8554b8fb52c9abf96d2f5790
Kapil Jugnu <kr_kapil@hotmail.com>
2025-05-13 11:06:38 +0530

    link for navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-  return <h1>Home page</h1>
+  return <>
+    <h1>Home page</h1>
+    Goto <Link to="/products">list of products</Link>
+  </>
 }
